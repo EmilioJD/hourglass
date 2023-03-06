dc052db8fe3970444fd9512c928ad87b4edab21c
Emilio De Leon <emiliojdeleon14@gmail.com>
2023-03-05 21:57:23 -0500

    hourglass logo

--- src/routes/timeline/+page.svelte
+++ src/routes/timeline/+page.svelte
@@ -7,9 +7,11 @@
 
 	import { goto } from '$app/navigation';
 	import { browser } from '$app/environment';
- 	import { writable } from 'svelte/store';
+	import { writable } from 'svelte/store';
 
 	import { votesArray } from '../../stores';
+
+	let img = '../../../hourglass.gif';
 
 	const len = 7 * 32;
 
@@ -23,18 +25,18 @@
 
 	function resetVotes() {
 		const empty = new Array(len).fill(0);
-		votesArray.update(n => empty)
+		votesArray.update((n) => empty);
 	}
-	import { timeZone } from '../+page.svelte'
+	import { timeZone } from '../+page.svelte';
 
-	let timeZoneOffset = parseInt(timeZone.substr(4,3)) + 5;
+	let timeZoneOffset = parseInt(timeZone.substr(4, 3)) + 5;
 	// console.log(timeZoneOffset);
 
 	function handleSave() {
 		// if (browser) {
 		// 	window.localStorage.setItem(currUserName, allTimeslots.toString());
 		// }
-		votesArray.update(n => updateVotes(n))
+		votesArray.update((n) => updateVotes(n));
 		var timerEnd = new Date();
 		var timeTaken = timerEnd.getTime() - timerStart.getTime();
 		console.log(`Name: ${currUserName}, Time Taken (in ms): ${timeTaken}`);
@@ -44,6 +46,7 @@
 		goto(`/`);
 	}
 </script>
+
 <!-- 
 <nav>
 	<a href="/">Start</a>
@@ -51,29 +54,44 @@
 </nav> -->
 
 <div class="app">
-	<h1>Hourglass</h1>
+	<div class="logo">
+		<!-- <img src={img} alt="hourglass" /> -->
+		<h1>Hourglass</h1>
+	</div>
+
 	<h3>Select Your Availability</h3>
-	<div class='saveDiv'>
-		<Button class='save' color='success' on:click={() => handleSave()}>Save and Logout</Button>
+	<div class="saveDiv">
+		<Button class="save" color="success" on:click={() => handleSave()}>Save and Logout</Button>
 	</div>
 	<Timeslots />
-	<hr/>
-	<Results/>
-	<br/>
+	<hr />
+	<Results />
+	<br />
 	<!-- <button on:click={()=>{console.log(mergedTimeslots, allTimeslots)}}>
         Print Selected Times
     </button> -->
 	<!-- <button on:click={() => handleSave()}>Save and Logout</button> -->
-	<br/>
-	<br/>
-	<button on:click={() => resetVotes()}>Reset Vote Count (press this + comment button out before presentation)</button>
+	<br />
+	<br />
+	<button on:click={() => resetVotes()}
+		>Reset Vote Count (press this + comment button out before presentation)</button
+	>
 </div>
 
 <style>
 	h1 {
 		font-size: 70px;
+	}
+	.logo {
+		display: flex;
+		justify-content: center;
+		align-items: center;
 		margin-top: 20px;
 		margin-bottom: 50px;
+	}
+	img {
+		width: 100px;
+		height: 100px;
 	}
 	.app {
 		position: relative;
@@ -85,7 +103,7 @@
 		justify-content: center;
 		box-sizing: border-box;
 		color: #cbcbcb;
-    	background-color: #090c17;
+		background-color: #090c17;
 	}
 
 	.saveDiv {
@@ -94,6 +112,6 @@
 		margin-bottom: -30px;
 	}
 	hr {
-		margin-top:100px;
+		margin-top: 100px;
 	}
 </style>
